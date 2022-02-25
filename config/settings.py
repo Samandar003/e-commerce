1c76b3ccf2e33dbad262372c34e22dd58f17eaa4
Samandar003 <samandar200527@gmail.com>
2022-02-25 20:37:01 +0500

    added templates

--- config/settings.py
+++ config/settings.py
@@ -109,6 +109,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR.joinpath('static')
 ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+MEDIA_URL = '/images/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
